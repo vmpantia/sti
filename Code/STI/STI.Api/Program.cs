a6537e181cf95e3bdb1e3b506cabbf1390c53972
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-04-25 19:07:43 +0800

    [+] Add STI.Api

---
+++ Code/STI/STI.Api/Program.cs
@@ -0,0 +1,34 @@
+using STI.DAT.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using STI.DAT.Contractors;
+using STI.DAT;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<StudentDbContext>(option => 
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
