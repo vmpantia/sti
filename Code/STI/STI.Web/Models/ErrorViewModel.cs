e4a8fbd1f7b57a09b715858177c13ca50dd3ac97
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-04-27 19:14:33 +0800

    [+] Add STI.Web

---
+++ Code/STI/STI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace STI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
