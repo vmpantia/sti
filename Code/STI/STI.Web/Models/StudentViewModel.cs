11694ba3c03e762b38f54ca0cfcfc50862eb6b7f
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-04-27 19:45:17 +0800

    [+] Add Student Index Page

---
+++ Code/STI/STI.Web/Models/StudentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using STI.BAL.Models.DTOs;
+
+namespace STI.Web.Models
+{
+    public class StudentViewModel
+    {
+        public List<StudentDTO> studentList { get; set; }
+        public StudentDTO inputStudent { get; set; }
+    }
+}
--- Code/STI/STI.Web/Program.cs
+++ Code/STI/STI.Web/Program.cs
@@ -1,14 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using STI.DAT.Contractors;
+using STI.DAT.DataAccess;
+using STI.DAT;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddDbContext<StudentDbContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Student/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -22,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Student}/{action=Index}/{id?}");
 
 app.Run();
